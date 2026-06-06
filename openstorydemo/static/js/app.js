4c14bb30c79b9ef5421917309f4aedae74460855
gautav2003 <270303649@yoobeestudent.ac.nz>
2026-06-06 15:46:24 +1200

    Add files via upload

---
+++ openstorydemo/static/js/app.js
@@ -0,0 +1,4 @@
+// Auto-dismiss flash messages
+document.querySelectorAll('.flash').forEach(el => {
+  setTimeout(() => { el.style.opacity = '0'; el.style.transition = 'opacity .5s'; setTimeout(() => el.remove(), 500); }, 4000);
+});
